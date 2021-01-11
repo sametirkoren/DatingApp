2ead42a951f30e5627543d457637da668512aa5b
Samet İrkören <sametirkoren@gmail.com>
2021-01-11 16:48:48 +0300

    update member

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
@@ -46,11 +47,22 @@ namespace DatingApp.API.Controllers
             if (user == null)
                 return NotFound($"{username} adına sahip bir kullanıcı yok.");
             return user;
+  
+        }
+
+        [HttpPut]
+
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            _mapper.Map(memberUpdateDto, user);
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
 
 
-
-
-           
+            return BadRequest("Failed to update user");
         }
     }
 }
